55b34837fc03b728156cb2f8b9dff83c14d9c690
Diwas Rimal <rimaldiwas10@gmail.com>
2024-05-10 16:46:27 +0545

    Don't parse body in GET requests

--- backend/routes/message.go
+++ backend/routes/message.go
@@ -3,34 +3,25 @@ package routes
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/diwasrimal/gochat/backend/api"
 	"github.com/diwasrimal/gochat/backend/db"
 	"github.com/diwasrimal/gochat/backend/types"
-	"github.com/diwasrimal/gochat/backend/utils"
 )
 
 // Gets messages among two users from database.
 // Should be used with authentication middleware
 func MessagesGet(w http.ResponseWriter, r *http.Request) api.Response {
 	userId := r.Context().Value("userId").(uint64)
-	body, err := utils.ParseJson(r.Body)
-	log.Printf("Message get request with body: %v\n", body)
+	pairId, err := strconv.Atoi(r.PathValue("pairId"))
 	if err != nil {
-		return api.Response{
-			Code:    http.StatusBadRequest,
-			Payload: types.Json{"message": "Couldn't parse request body as json"},
-		}
-	}
-	id, ok := body["pairId"].(float64)
-	if !ok {
 		return api.Response{
 			Code:    http.StatusBadRequest,
 			Payload: types.Json{"message": "Invalid data about chat pair"},
 		}
 	}
-	pairId := uint64(id)
-	messages, err := db.GetMessagesAmong(userId, pairId)
+	messages, err := db.GetMessagesAmong(userId, uint64(pairId))
 	if err != nil {
 		log.Printf("Error getting messsages among (%v, %v) from db: %v\n", userId, pairId, err)
 		return api.Response{
