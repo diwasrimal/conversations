10c6a1e8b253906bb43b4274a709ddcc9374ee0a
Diwas Rimal <rimaldiwas10@gmail.com>
2024-05-10 17:27:54 +0545

    Send json response during error too

--- backend/middleware/use_json.go
+++ backend/middleware/use_json.go
@@ -3,20 +3,28 @@ package middleware
 import (
 	"mime"
 	"net/http"
+
+	"github.com/diwasrimal/gochat/backend/types"
+	"github.com/diwasrimal/gochat/backend/utils"
 )
 
 func UseJson(nextHandler http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		mt, _, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
 		if err != nil {
-			http.Error(w, "Malformed Content-Type header", http.StatusBadRequest)
+			utils.SendJsonResp(w, http.StatusBadRequest, types.Json{"messsage": "Malformed Content-Type header"})
 			return
 		}
 		if mt != "application/json" {
-			http.Error(w, "Content-Type must be application/json", http.StatusUnsupportedMediaType)
+			utils.SendJsonResp(
+				w,
+				http.StatusUnsupportedMediaType,
+				types.Json{"messsage": "Content-Type must be application/json"},
+			)
 			return
 		}
 		nextHandler.ServeHTTP(w, r)
+
 	}
 	return http.HandlerFunc(fn)
 }
