fb61ab992fd8ae84589fe5258d3b1da93e329fc0
Diwas Rimal <rimaldiwas10@gmail.com>
2024-05-13 17:14:44 +0545

    Fix spelling in response

--- backend/middleware/use_json.go
+++ backend/middleware/use_json.go
@@ -12,14 +12,14 @@ func UseJson(nextHandler http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		mt, _, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
 		if err != nil {
-			utils.SendJsonResp(w, http.StatusBadRequest, types.Json{"messsage": "Malformed Content-Type header"})
+			utils.SendJsonResp(w, http.StatusBadRequest, types.Json{"message": "Malformed Content-Type header"})
 			return
 		}
 		if mt != "application/json" {
 			utils.SendJsonResp(
 				w,
 				http.StatusUnsupportedMediaType,
-				types.Json{"messsage": "Content-Type must be application/json"},
+				types.Json{"message": "Content-Type must be application/json"},
 			)
 			return
 		}
