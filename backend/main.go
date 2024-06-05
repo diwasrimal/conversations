9d465a7685ee66f96d7ee7d80f1baec8c3482b64
Diwas Rimal <rimaldiwas10@gmail.com>
2024-06-05 20:32:53 +0545

    Remove unused route /api/profile

--- backend/main.go
+++ backend/main.go
@@ -19,32 +19,32 @@ func main() {
 
 	handlers := map[string]http.Handler{
 		"GET /api/logout":            mw.UseJson(api.MakeHandler(routes.LogoutGet)),
-		"GET /api/tmp":               mw.UseJson(api.MakeHandler(routes.TmpGet)),
-		"POST /api/login":            mw.UseJson(api.MakeHandler(routes.LoginPost)),
 		"GET /api/login-status":      mw.UseAuth(api.MakeHandler(routes.LoginStatusGet)),
-		"POST /api/register":         mw.UseJson(api.MakeHandler(routes.RegisterPost)),
-		"GET /api/profile":           mw.UseAuth(mw.UseJson(api.MakeHandler(routes.ProfileGet))),
-		"PUT /api/profile":           mw.UseAuth(mw.UseJson(api.MakeHandler(routes.ProfilePut))),
+		"GET /api/users/{id}":        mw.UseAuth(mw.UseJson(api.MakeHandler(routes.UsersGet))),
 		"GET /api/chat-partners":     mw.UseAuth(mw.UseJson(api.MakeHandler(routes.ChatPartnersGet))),
 		"GET /api/search":            mw.UseAuth(mw.UseJson(api.MakeHandler(routes.SearchGet))),
 		"GET /api/messages/{pairId}": mw.UseAuth(mw.UseJson(api.MakeHandler(routes.MessagesGet))),
+		"GET /api/friends":           mw.UseAuth(mw.UseJson(api.MakeHandler(routes.FriendsGet))),
+		"GET /api/friend-requestors": mw.UseAuth(mw.UseJson(api.MakeHandler(routes.FriendRequestorsGet))),
+		"GET /ws":                    mw.UseAuth(http.HandlerFunc(routes.WSHandleFunc)),
+		"GET /api/tmp":               mw.UseJson(api.MakeHandler(routes.TmpGet)),
 
 		"GET /api/friendship-status/{targetId}": mw.UseAuth(mw.UseJson(api.MakeHandler(routes.FriendshipStatusGet))),
-		"POST /api/friend-requests":             mw.UseAuth(mw.UseJson(api.MakeHandler(routes.FriendRequestPost))),
-		"DELETE /api/friend-requests":           mw.UseAuth(mw.UseJson(api.MakeHandler(routes.FriendRequestDelete))),
-		"GET /api/friends":                      mw.UseAuth(mw.UseJson(api.MakeHandler(routes.FriendsGet))),
-		"POST /api/friends":                     mw.UseAuth(mw.UseJson(api.MakeHandler(routes.FriendPost))),
-		"DELETE /api/friends":                   mw.UseAuth(mw.UseJson(api.MakeHandler(routes.FriendDelete))),
-		"GET /api/friend-requestors":            mw.UseAuth(mw.UseJson(api.MakeHandler(routes.FriendRequestorsGet))),
 
-		"GET /ws": mw.UseAuth(http.HandlerFunc(routes.WSHandleFunc)),
+		"POST /api/login":           mw.UseJson(api.MakeHandler(routes.LoginPost)),
+		"POST /api/register":        mw.UseJson(api.MakeHandler(routes.RegisterPost)),
+		"POST /api/friend-requests": mw.UseAuth(mw.UseJson(api.MakeHandler(routes.FriendRequestPost))),
+		"POST /api/friends":         mw.UseAuth(mw.UseJson(api.MakeHandler(routes.FriendPost))),
+
+		"DELETE /api/friend-requests": mw.UseAuth(mw.UseJson(api.MakeHandler(routes.FriendRequestDelete))),
+		"DELETE /api/friends":         mw.UseAuth(mw.UseJson(api.MakeHandler(routes.FriendDelete))),
 	}
 	mux := http.NewServeMux()
 	for route, handler := range handlers {
 		mux.Handle(route, handler)
 	}
 
-	// File server to serve frontend build files at /build
+	// File server to serve frontend build files
 	mux.Handle("/", http.FileServer(http.Dir("./dist")))
 
 	finalHandler := cors.AllowAll().Handler(mux)
